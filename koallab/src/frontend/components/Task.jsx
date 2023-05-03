1c22be8edca3491715243cdfe3663b02a363c889
asin819 <asin819@aucklanduni.ac.nz>
2023-05-03 20:37:10 +1200

    Component: Task card

---
+++ koallab/src/frontend/components/Task.jsx
@@ -0,0 +1,47 @@
+import React from "react";
+import "./Task.css";
+import informationIcon from "../assets/info.png";
+
+const Task = ({
+  TaskTitle,
+  description,
+  username,
+  TaskStatus,
+  TaskImportance,
+}) => {
+  let textColor = "default";
+  if ((TaskImportance = "Non-Important")) {
+    textColor = "nonImportant";
+  } else if ((TaskImportance = "Important")) {
+    textColor = "important";
+  }
+  return (
+    <div className="Task">
+      <h2>{TaskTitle}</h2>
+      <hr></hr>
+      <p>Created at: </p>
+      <p>Assigned to: {username}</p>
+
+      <h3>{description}</h3>
+
+      <p>Status: {TaskStatus}</p>
+      <p className={textColor}>{TaskImportance}</p>
+      <p>Estimated time: </p>
+      <div className="Labels">
+        <p>Task labels here</p>
+      </div>
+
+      <div className="InfoButton">
+        <button>
+          <img
+            style={{ width: "30px", height: "30px" }}
+            src={informationIcon}
+            alt="View all information"
+          />
+        </button>
+      </div>
+    </div>
+  );
+};
+
+export default Task;
