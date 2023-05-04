cef372ede0ec8867881c498c4551f837573a422b
UoAres <syan408@aucklanduni.ac.nz>
2023-05-05 00:09:39 +1200

    finish 1-15 api by Shuiling Yang

--- routes/users.js
+++ routes/users.js
@@ -18,5 +18,64 @@ router.post('/login', user.login);
  */
 router.post('/register', user.register);
 
+/**
+ * Modificate user information
+ */
+router.post('/modifyUserInfo', user.modifyUserInfo);
+
+/**
+ * Logout
+ */
+router.post('/logout', user.logout);
+
+/**
+ * Delete user from database
+ */
+router.post('/deleteUser', user.deleteUser);
+
+/**
+ * Get user information from group
+ */
+router.post('/getGroupUserList', user.getGroupUserList);
+
+/**
+ * Get user information from project
+ */
+router.post('/getProjectUserList', user.getProjectUserList);
+
+/**
+ * Create a new group
+ */
+router.post('/newGroup', user.newGroup);
+
+/**
+ * Disband a group
+ */
+router.post('/disbandGroup', user.disbandGroup);
+
+/**
+ * Modify group information
+ */
+router.post('/modifyGroupInfo', user.modifyGroupInfo);
+
+/**
+ * add a user to group
+ */
+router.post('/addUserToGroup', user.addUserToGroup);
+
+/**
+ * remove user from group
+ */
+router.post('/removeUserFromGroup', user.removeUserFromGroup);
+
+/**
+ * modify user role from group
+ */
+router.post('/modifyUserRoleFromGroup', user.modifyUserRoleFromGroup);
+
+/**
+ * get group information
+ */
+router.get('/getGroupInfo', user.getGroupInfo);
 
 module.exports = router;
