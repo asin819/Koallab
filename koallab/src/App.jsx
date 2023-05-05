72c62028c1f8121ee03508c756c8d1ce371added
asin819 <asin819@aucklanduni.ac.nz>
2023-05-06 02:33:11 +1200

    Page: Login page

--- koallab/src/App.jsx
+++ koallab/src/App.jsx
@@ -3,6 +3,8 @@ import Task from './frontend/components/Task.jsx'
 import TaskCompact from './frontend/components/TaskCompact.jsx'
 import TeamCard from './frontend/components/TeamCard.jsx'
 import Navbar from './frontend/components/Navbar.jsx'
+import Homepage from './frontend/pages/homepage/homepage.jsx'
+import Login from './frontend/pages/login/Login.jsx'
 
 import './App.css'
 
@@ -10,29 +12,30 @@ function App() {
   const [count, setCount] = useState(0)
 
   return (
+    
     <div>
-      <div>
-      <Navbar/>
-      </div>
       
-       {/* <Task
+      {/* <Homepage/> */}
+      <Login/>
+{/*       
+       <Task
         TaskTitle="Make a task card"
         description="Make a component that will be used for the task as a card, refer to the DB provided to make sure you have all the information you need and put that in."
         username="abc"
         TaskStatus="new"
         TaskImportance="Important"
-      /> */}
+      />
 
-      {/* <TaskCompact
+      <TaskCompact
         TaskTitle="Make a task card"
         TaskImportance="Important"
-      /> */}
+      />
 
       <TeamCard
         TeamName="Khaki Koallas"
-      />
-    </div>
-  
+      /> */}
+     </div>
+    
   )
 }
 
