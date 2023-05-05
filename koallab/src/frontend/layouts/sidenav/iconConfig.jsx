2aa9db40641252a0fa01940ee91fc06f00b6dee7
asin819 <asin819@aucklanduni.ac.nz>
2023-05-06 04:06:50 +1200

    Layout: Side nav partially complete

---
+++ koallab/src/frontend/layouts/sidenav/iconConfig.jsx
@@ -0,0 +1,45 @@
+import ChartBarIcon from '@heroicons/react/24/solid/ChartBarIcon';
+import CogIcon from '@heroicons/react/24/solid/CogIcon';
+import ShoppingBagIcon from '@heroicons/react/24/solid/ShoppingBagIcon';
+import UserIcon from '@heroicons/react/24/solid/UserIcon';
+import UsersIcon from '@heroicons/react/24/solid/UsersIcon';
+import { SvgIcon } from '@mui/material';
+
+export const items = [
+  {
+    title: 'Overview',
+    path: '/',
+    icon: (
+      <SvgIcon fontSize="small">
+        <ChartBarIcon />
+      </SvgIcon>
+    )
+  },
+  {
+    title: 'Groups',
+    path: '/',
+    icon: (
+      <SvgIcon fontSize="small">
+        <UsersIcon />
+      </SvgIcon>
+    )
+  },
+  {
+    title: 'Account',
+    path: '/',
+    icon: (
+      <SvgIcon fontSize="small">
+        <UserIcon />
+      </SvgIcon>
+    )
+  },
+  {
+    title: 'Settings',
+    path: '/',
+    icon: (
+      <SvgIcon fontSize="small">
+        <CogIcon />
+      </SvgIcon>
+    )
+  },
+];
