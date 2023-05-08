3dc6b822bbd7d7805ec1f1a1a6bc60bc5ad1616f
asin819 <asin819@aucklanduni.ac.nz>
2023-05-08 20:01:52 +1200

    Feat: Routes

--- koallab/src/App.jsx
+++ koallab/src/App.jsx
@@ -1,18 +1,24 @@
 import React from "react";
-import Login from './frontend/pages/login/Login.jsx'
+import { BrowserRouter, Routes, Route, Link } from 'react-router-dom';
+import Login from './frontend/pages/login/Login.jsx';
+import Homepage from './frontend/pages/homepage/Homepage.jsx';
+import Group from './frontend/pages/group/Group.jsx'
+import Project from './frontend/pages/project/Project.jsx'
+import UserProfile from './frontend/pages/userProfile/UserProfile.jsx'
 import './App.css'
 
 function App() {
 
   return (
-    
-    <div>
-      
-      
-      <Login/>
-      
-     </div>
-    
+    <BrowserRouter>
+    <Routes>
+      <Route path="/login" element={<Login/>}/>
+      <Route path="/" element={<Homepage/>}/>
+      <Route path="/group" element={<Group/>}/>
+      <Route path="/project" element={<Project/>}/>
+      <Route path="/userProfile" element={<UserProfile/>}/>
+    </Routes>
+    </BrowserRouter>
   )
 }
 
