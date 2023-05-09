6d5ffccb3b0c6f5e6cf15d9a8360e73e33a75c53
jessicafenwick <jessicajacksonfenwick@gmail.com>
2023-05-09 23:43:27 +1200

    base for profile page

--- koallab/src/frontend/pages/project/Project.jsx
+++ koallab/src/frontend/pages/project/Project.jsx
@@ -1,9 +1,26 @@
 import React from "react";
-import './Project.css'
+import './Project.css';
+import UserIcon from '@heroicons/react/24/solid/UserIcon';
 
 const Project = () => {
 return(
-<p>Project</p>
+<Container>
+    <Box
+        sx={{
+          boxShadow: 3,
+          borderRadius: 2,
+          px: 4,
+          py: 6,
+          marginTop: 8,
+          display: "flex",
+          flexDirection: "column",
+          alignItems: "center",
+        }}
+    >
+        <UserIcon/> 
+    </Box>
+
+</Container>
 
 )
 
--- koallab/src/frontend/pages/userProfile/UserProfile.jsx
+++ koallab/src/frontend/pages/userProfile/UserProfile.jsx
@@ -1,10 +1,36 @@
 import React from "react";
-import './UserProfile.css'
+import './UserProfile.css';
+import Container from "@mui/material/Container";
+import Box from "@mui/material/Box";
+import UserIcon from '@heroicons/react/24/solid/UserIcon';
+import { Button } from "@mui/material";
+import UserInfoCard from "../../components/userInfoCard";
+import IdentityCard from "../../components/IdentityCard";
 
 const UserProfile = () => {
 return(
-<p>User Profile</p>
+<Container>
 
+    <IdentityCard/>
+
+    <UserInfoCard/>
+
+    <Box
+        sx={{
+          borderRadius: 2,
+          padding: 2,
+          marginTop: 2,
+          display: "flex",
+          flexDirection: "column",
+          
+        }}
+    >
+        <Button variant="outlined" color="error">
+            Delete User
+        </Button>
+    </Box>
+    
+</Container>
 )
 
 };
