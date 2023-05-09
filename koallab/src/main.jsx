bdd8025637b2634f376c864b384cec64ce212984
ema846 <ema846@aucklanduni.ac.nz>
2023-05-10 02:24:51 +1200

    setup the API calls with redux

--- koallab/src/main.jsx
+++ koallab/src/main.jsx
@@ -2,7 +2,11 @@ import React from 'react'
 import ReactDOM from 'react-dom/client'
 import App from './App.jsx'
 import './index.css'
+import { Provider } from 'react-redux'
+import {createStore,applyMiddleware} from 'redux'
+// import rootReducer from './reducer'
 
+const store = createStore(rootReducer)
 ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
     <App />
