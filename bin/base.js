22309272cbc6cde62a06708a63766a8f89947439
UoAres <syan408@aucklanduni.ac.nz>
2023-05-12 11:44:47 +1200

    fix bug

--- bin/base.js
+++ bin/base.js
@@ -74,8 +74,8 @@ this.checkReq = async function (req, res, next) {
                 if (req.query!=null){
                     req.body = Object.assign({}, req.query, req.body);
                 }
-                if (req.data && req.data.token) {
-                    await global.db.modUser.find({ "authorizationtoken": req.data.token }).then(async (docs) => {
+                if (req.body && req.body.token) {
+                    await global.db.modUser.find({ "authorizationtoken": req.body.token }).then(async (docs) => {
                         if (docs.length > 0 && docs[0] != null) {
                             let authorizationvalidityexpirationdate = docs[0].authorizationvalidityexpirationdate;
                             if (authorizationvalidityexpirationdate > new Date()) {
