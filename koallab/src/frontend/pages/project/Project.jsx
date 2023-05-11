82b3cf6a247be893bb32590cc6579f97644351a1
jessicafenwick <jessicajacksonfenwick@gmail.com>
2023-05-11 23:42:43 +1200

    popover

--- koallab/src/frontend/pages/project/Project.jsx
+++ koallab/src/frontend/pages/project/Project.jsx
@@ -1,92 +1,112 @@
 import React from "react";
 import './Project.css'
-import { Box, Container, Divider } from "@mui/material";
+import { Box, Container, Divider, Button, Popover, Typography } from "@mui/material";
+import { useState } from "react";
 import TaskCompact from "../../components/old/TaskCompact";
+import PencilIcon from '@heroicons/react/24/solid/PencilIcon';
 
-const Project = () => {
+function Project() {
+
+    const [editingOpen, setEditingOpen] = useState(false); 
+
 return(
-<Container>
+<Container> 
     <Box
     classname = "headerBox"
     sx={{
         backgroundColor: "primary.light"
     }}>
-        <h1> Project Title </h1>
-        <p> Project Info</p>
+       <h1> Project Title </h1> 
+      <p> Project Info</p> 
+        <Button variant="contained" onClick={() => setEditingOpen(true)}>
+          Edit
+        </Button>
+        <Popover
+            open={editingOpen}
+            onClose={() => setEditingOpen(false)}
+            anchorOrigin={{
+                vertical: 'bottom',
+                horizontal: 'left',
+            }}
+            >
+                <p>plz</p>
+        </Popover>
+    </Box> 
 
-    </Box>
+
+<Box
+classname = "completenessBox"
+sx={{
+    backgroundColor: "primary.light"
+}}>
+    <h3> x% Complete </h3>
+</Box>
+
+<Divider />
+
+<Box
+classname = "memberBox"
+sx={{
+    backgroundColor: "primary.light"
+}}>
+    <h2> Members </h2>
 
     <Box
-    classname = "completenessBox"
+    classname = "taskLists"
     sx={{
-        backgroundColor: "primary.light"
+        display: "flex",
+        flexDirection: "row"
     }}>
-        <h3> x% Complete </h3>
+        Insert list of members
     </Box>
+</Box>
 
-    <Divider />
-
+<Box
+classname = "tasks"
+sx={{
+    backgroundColor: "primary.light"
+}}>
+    <h2>Tasks</h2>
     <Box
-    classname = "memberBox"
+    classname = "taskLists"
     sx={{
-        backgroundColor: "primary.light"
+        display: "flex",
+        flexDirection: "row"
     }}>
-        <h2> Members </h2>
 
-        <Box
-        classname = "taskLists"
+      <Box
+        classname = "toDoTasks"
         sx={{
-            display: "flex",
-            flexDirection: "row"
+            backgroundColor: "secondary.light"
         }}>
-            Insert list of members
+            <h3>To Do</h3>
+            <TaskCompact TaskTitle="To Do" TaskImportance="Important"></TaskCompact>
         </Box>
-    </Box>
-
-    <Box
-    classname = "tasks"
-    sx={{
-        backgroundColor: "primary.light"
-    }}>
-        <h2>Tasks</h2>
         <Box
-        classname = "taskLists"
+        classname = "inProgressTasks"
         sx={{
-            display: "flex",
-            flexDirection: "row"
+            backgroundColor: "secondary.light"
         }}>
-
-          <Box
-            classname = "toDoTasks"
-            sx={{
-                backgroundColor: "secondary.light"
-            }}>
-                <h3>To Do</h3>
-                <TaskCompact TaskTitle="To Do" TaskImportance="Important"></TaskCompact>
-            </Box>
-            <Box
-            classname = "inProgressTasks"
-            sx={{
-                backgroundColor: "secondary.light"
-            }}>
-                <h3>In Progress</h3>
-                <TaskCompact TaskTitle="In Progress" TaskImportance="Important"></TaskCompact>
-            </Box>
-            <Box
-            classname = "completedTasks"
-            sx={{
-                backgroundColor: "secondary.light",
-            }}>
-                <h3>Completed</h3>
-                <TaskCompact TaskTitle="Completed" TaskImportance="Important"></TaskCompact>
-            </Box>  
+            <h3>In Progress</h3>
+            <TaskCompact TaskTitle="In Progress" TaskImportance="Important"></TaskCompact>
         </Box>
-        
-
+        <Box
+        classname = "completedTasks"
+        sx={{
+            backgroundColor: "secondary.light",
+        }}>
+            <h3>Completed</h3>
+            <TaskCompact TaskTitle="Completed" TaskImportance="Important"></TaskCompact>
+        </Box>  
     </Box>
     
 
+</Box>
+    
+
 </Container>
+   
+
 
 )
 
